74729cb552cb59c968c4ecb6cc27e928f6fe6e4a
jasmine aqilah <jasmine_aqilah_29rpl@student.smktelkom-mlg.sch.id>
2022-03-15 17:31:45 +0700

    Latihan UKL Java

---
+++ LATIHAN1.java
@@ -0,0 +1,16 @@
+public class LATIHAN1 
+{
+    void cetak()
+    {
+    System.out.println("---------------");
+    System.out.println("Hello World");
+    System.out.println("SELAMAT DATANG");
+    System.out.println("---------------");
+    }
+public static void main(String[]args){
+    LATIHAN1 hello = new LATIHAN1();
+    hello.cetak();
+    
+
+  }
+}
